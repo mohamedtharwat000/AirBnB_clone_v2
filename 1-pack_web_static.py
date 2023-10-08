73d848d519f1cb1a4af1918959b6b8733c4c34bf
LORDyyyyy <mlord6271@gmail.com>
2023-10-08 17:21:57 +0300

    1-pack_web_static.py

---
+++ 1-pack_web_static.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" doc """
+from fabric.api import local
+from datetime import datetime
+
+def do_pack():
+    """ Compress before sending """
+
+    try:
+        local("mkdir -p versions")
+
+        now = datetime.now()
+        time = now.strftime("%Y%m%d%H%M%S")
+
+        archive_name = f"web_static_{time}.tgz"
+        local(f"tar -cvzf versions/{archive_name} web_static")
+
+        return (f"versions/{archive_name}")
+
+    except Exception as e:
+        return None

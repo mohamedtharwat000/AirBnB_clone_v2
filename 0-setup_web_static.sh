f4eb8c811b5aac96adc37306fad93643ec81b538
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2023-10-11 22:42:53 +0300

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -27,6 +27,6 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
 
-sudo sed -i "/server_name _;/a \n\tlocation /hbnb_static {alias /data/web_static/current/;}" /etc/nginx/sites-available/default
+sudo sed -i "/server_name _;/a \ \n\tlocation /hbnb_static {alias /data/web_static/current/;}" /etc/nginx/sites-available/default
 
 sudo nginx -s reload

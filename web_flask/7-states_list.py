97a3a120b77abd9438f5da3ed4ed8e34bf9cd708
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2023-10-27 16:43:23 +0200

    Update 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python3
-"""List of states on HTML states_list"""
-
+""" start point """
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-
 app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """ """
+    storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """display a HTML page: (inside the tag BODY)"""
-    sortedlist = sorted(storage.all(
-        State).values(), key=lambda x: x.name)
-    return render_template("7-states_list.html", sorted_states_list=sortedlist)
-
-
-@app.teardown_appcontext
-def terminate(exc):
-    """close the storage"""
-    storage.close()
+    """ """
+    states = sorted(storage.all(State).values(), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
 
 
 if __name__ == '__main__':
-    """start the server"""
     app.run(host='0.0.0.0', port=5000)

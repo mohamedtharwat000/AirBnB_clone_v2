70fc7a96eee292e99fa7489d23cd5514676f65b9
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2023-10-27 16:33:14 +0200

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from os import getenv
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 from models.city import City
@@ -9,11 +9,6 @@ from models.city import City
 
 class State(BaseModel, Base):
     """ State class """
-
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'states'
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state", cascade="delete")
 
     if getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
@@ -25,3 +20,8 @@ class State(BaseModel, Base):
                 if city.state_id == self.id:
                     city_list.append(city)
             return city_list
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state", cascade="delete")

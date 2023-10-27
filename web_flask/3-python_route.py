529a692bf53a03bcc0d7c32eab930b0a80b44f85
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2023-10-27 14:56:19 +0200

    Update 3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -26,7 +26,8 @@ def c_text(text):
     return f"C {text}"
 
 
-@app.route('/python/<text>', strict_slashes=False)
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
 def python_text(text="is cool"):
     """return python <text>!."""
     text = text.replace("_", " ")

7a0ec24706da1f33a3990ec0915054e9c6331e1e
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2023-10-27 16:35:01 +0200

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,27 +1,26 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from os import getenv
-from sqlalchemy import Column, String
+
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
-from models.city import City
+from os import getenv
 
 
 class State(BaseModel, Base):
-    """ State class """
+    """State class."""
 
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', cascade='delete', backref='state')
     if getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
         def cities(self):
-            """Return list of City objects from storage"""
+            """Cities getter in case of using file storage."""
+            from models.city import City
             from models import storage
-            city_list = []
+            cities_list = []
             for city in storage.all(City).values():
                 if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
-
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'states'
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state", cascade="delete")
+                    cities_list.append(city)
+            return cities_list

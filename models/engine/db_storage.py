2987f5d08f40224ade6dcfb48877b07b9b300d16
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2023-10-27 16:30:25 +0200

    Update db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -3,7 +3,7 @@
 from os import getenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
-from models.base_model import BaseModel, Base
+from models.base_model import Base
 from models.city import City
 from models.place import Place
 from models.state import State
